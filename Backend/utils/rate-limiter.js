4263e8c79e85d79d4914a8865246019ed0810052
fizzaalwani <fizzaokok@gmail.com>
2025-09-05 16:17:00 +0500

    gitignore replaced

--- Backend/utils/rate-limiter.js
+++ Backend/utils/rate-limiter.js
@@ -1,8 +1,8 @@
 const rateLimit=require('express-rate-limit')
 
 const limiter=rateLimit({
-    windowMs:1*60*1000,
-    max:15,
+    windowMs:15*60*1000,
+    max:20,
     message:"Limit reached .Please try again after some time",
     statusCode:429
 })
